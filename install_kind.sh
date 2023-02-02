b0ec8a651124694512b8d511c60d28a56bd689a1
Aan Agustiono <93072275+aan-agustiono@users.noreply.github.com>
2023-02-03 02:18:21 +0700

    Update install_kind.sh

--- install_kind.sh
+++ install_kind.sh
@@ -1,6 +1,8 @@
-#
+# 
 #
 # https://kind.sigs.k8s.io/docs/user/quick-start/#installation
+# kind is a tool for running local Kubernetes clusters using Docker container “nodes”.
+# kind was primarily designed for testing Kubernetes itself, but may be used for local development or CI.
 #
 
 # Binary mode
