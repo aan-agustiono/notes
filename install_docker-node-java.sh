a96481e1a7a16864dae26f9bff3922a272194fd6
Aan Agustiono <93072275+aan-agustiono@users.noreply.github.com>
2023-02-02 19:03:01 +0700

    Update install_docker-node-java.sh
    Tambahan Installation base OS Debian

--- install_docker-node-java.sh
+++ install_docker-node-java.sh
@@ -19,5 +19,9 @@ sudo apt update
 sudo apt install nodejs npm -y
 
 
-#   All in one script command :
+# Install base Ubuntu - All in one script command :
 sudo apt update && sudo apt-get install -y ca-certificates curl gnupg lsb-release && sudo mkdir -p /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt update && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y && sudo apt install default-jdk -y && sudo apt update && sudo apt install nodejs npm -y && sudo apt update && sudo apt upgrade -y
+
+
+# Install base Debian - All in one script 
+sudo apt update && sudo apt-get install -y ca-certificates curl gnupg lsb-release && sudo mkdir -p /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt update && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y && sudo apt install default-jdk -y && sudo apt update && sudo apt install nodejs npm -y && sudo apt update && sudo apt upgrade -y
