67ff18dacf5fb7a5a0995d3b8bdc7bc59a9bbfff
Aan Agustiono <93072275+aan-agustiono@users.noreply.github.com>
2023-03-17 14:59:26 +0700

    Create

---
+++ Linux/fresh_ubuntu2204_install.sh
@@ -0,0 +1,16 @@
+
+
+
+Step 01 - Install OS
+
+Step 02 - add ethernet config
+
+Step 03 - Remove snap & econfigure snap
+
+Step 04 - apt update 
+
+Step 05 - apt install nmap vim 
+
+Step -6 - apt update & apt upgrade
+
+
